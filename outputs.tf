c2c7ad7d48405f5235da8115acc2b63763e10835
Ajaya Loya <ajaykumar.loya@gmail.com>
2021-02-10 16:28:35 -0800

    Fixed Issue#1 that was reported.
    https://github.com/devops-made-easy/terraform-aws-security-group/issues?q=is%3Aissue+is%3Aopen+author%3ANuxij

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "security_group_id" {
-  value = aws_security_group.default.id
+  value = aws_security_group.sg.id
   description = "Security Group ID"
 }
 
 output "security_group_arn" {
-  value = aws_security_group.default.arn
+  value = aws_security_group.sg.arn
   description = "ARN of the security group."
 }
 
 output "ingress_rules" {
-  value = aws_security_group.default.ingress
+  value = aws_security_group.sg.ingress
   description = "All your ingress rules."
 }
 
 output "egress_rules" {
-  value = aws_security_group.default.egress
+  value = aws_security_group.sg.egress
   description = "All your egress rules."
 }
